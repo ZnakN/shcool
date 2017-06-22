504aaa8c304957410144982fccaaca793ee61a18
Aleksandr Neroznak <a.neroznak@itbsg.eu>
2017-06-22 11:39:03 +0300

    fix layout

---
+++ resources/views/layouts/custom.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Etiquette school - школа етикету Наталії Адаменко. Експерт з етикету і дипломатичного протоколу,
+      професійний бізнес-тренер.</title>
+    <meta name="description" content="Etiquette school - школа етикету Наталії Адаменко. Експерт з етикету і дипломатичного протоколу, професійний бізнес-тренер.">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <meta property="og:image" content="path/to/adamenko.png">
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/favicon/apple-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+
+
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}" media="screen" >
+    <link rel="stylesheet" href="{{asset('css/main.min.css')}}">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
+
+
+    <script>
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel(
+    {
+      nav: true,
+      items: 4,
+      autoWidth: false,
+      margin: 10,
+      navText: ["<img src='img/left.png'>", "<img src='img/right.png'>"]
+    });
+
+
+
+
+});
+    </script>
+
+
+    <script>
+      (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+          m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+      ga('create', 'UA-71261617-1', 'auto');
+      ga('send', 'pageview');
+
+
+
+    </script>
+    <script>
+      var rus_lang = {
+        "processing": "Подождите...",
+        "search": "Поиск:",
+        "lengthMenu": "Показать _MENU_ записей",
+        "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+        "infoEmpty": "Записи с 0 до 0 из 0 записей",
+        "infoFiltered": "(отфильтровано из _MAX_ записей)",
+        "infoPostFix": "",
+        "loadingRecords": "Загрузка записей...",
+        "zeroRecords": "Записи отсутствуют.",
+        "emptyTable": "В таблице отсутствуют данные",
+        "paginate": {
+          "first": "Первая",
+          "previous": "Предыдущая",
+          "next": "Следующая",
+          "last": "Последняя"
+        },
+        "aria": {
+          "sortAscending": ": активировать для сортировки столбца по возрастанию",
+          "sortDescending": ": активировать для сортировки столбца по убыванию"
+        }
+      };
+    </script>  
+
+
+    <style>
+      .owl-prev {
+        /*    width: 35px;
+            height: 35px;*/
+        position: absolute;
+        top: 36%;
+        left: -35px;
+        display: block!IMPORTANT;
+        border:0px solid black;
+      }
+
+      .owl-next {
+        /*       width: 35px;
+            height: 35px;*/
+        position: absolute;
+        top: 36%;
+        right: -35px;
+        display: block!IMPORTANT;
+        border:0px solid black;
+      }
+      /*.owl-prev i, .owl-next i {transform : scale(1,6); color: black;}   */
+
+
+    </style>
+
+
+
+  </head>
+
+  <body>
+
+    <header>
+      <div class="container">	
+        <div class="row">
+          <div class="header__logo-container logo-container col-sm-offset-1 col-md-offset-1 col-lg-offset-2">
+            <h1 class="header__logo logo">Etiquette School</h1>
+            <h3 class="header__topic topic">ПЕРША ШКОЛА<br>ДІЛОВИХ МАНЕР<br>ТА КОМУНІКАЦІЙ</h3>
+          </div>
+        </div>
+        <div class="header__btn-container btn-container">
+          <a class="header__btn btn" href="https://www.facebook.com/etiq.school/?fref=ts" target="_blank" role="button">БІЛЬШЕ ІНФОРМАЦІЇ НА <span class="header__btn--fb">FACEBOOK</span></a>
+        </div>
+      </div>
+    </header>
+
+        @yield('content')
+
+        
+        <footer>
+          <div class="container">
+            <ul class="footer-menu">
+              <li>
+                <div class="logo-container">
+                  <h2 class="logo">ES</h2>
+                  <h3 class="topic">label of your success</h3>
+                </div>
+              </li>
+              <li>
+                <a href="mailto:info@etiqschool.com.ua" target="_blank" class="btn btn--large" role="button">Написати нам</a>
+              </li>
+              <li><a href="tel:0444667476">044 466 74 76</a><br>
+                <a href="mailto:info@etiqschool.com.ua">info@etiqschool.com.ua</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+        <!-- Google Code for &#1047;&#1072;&#1088;&#1077;&#1075;&#1080;&#1089;&#1090;&#1088;&#1080;&#1088;&#1086;&#1074;&#1072;&#1090;&#1100;&#1089;&#1103; Conversion Page -->
+        <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 932834883;
+        var google_conversion_language = "en";
+        var google_conversion_format = "3";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "FNECCOOOpWIQw9znvAM";
+        var google_remarketing_only = false;
+        /* ]]> */
+        </script>
+
+
+
+        <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+        </script>
+        <noscript>
+        <div style="display:inline;">
+          <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/932834883/?label=FNECCOOOpWIQw9znvAM&amp;guid=ON&amp;script=0"/>
+        </div>
+        </noscript>
+        <!-- end google code -->	
+  </body>
+</html>
--- resources/views/mail/cardfail.blade.php
+++ resources/views/mail/cardfail.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.custom')
 
 @section('content')  
+ 
 <div class="row">
   <div class="col-lg-5 col-lg-offset-4">
-    <br>
-    <br>
-    <br>
+         <br>
+         <br>
+         <br>
          <b style="margin-left:170px;">Дякуємо!</b><br>
-         <b>Оплата пройшла успішно</b><br>
+         <b>Нажаль оплата не була здійснена.</b><br>
          <b>Підтвердження було вислано на ваш e-mail</b><br>
          <br>
          <br>
          <br>
   </div>
-</div>
+  </div>
 @endsection
