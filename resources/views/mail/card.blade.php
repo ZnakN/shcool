b3d94ad95723a52b976b6e0594723d2bba08be21
TanyaGrek <3334847@gmail.com>
2017-07-04 10:28:03 +0300

    fix layout
    fix layout

--- resources/views/mail/card.blade.php
+++ resources/views/mail/card.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')  
 <div class="row">
-  <div class="col-lg-5 col-lg-offset-4">
+  <div class="successful-payment">
     <br>
     <br>
     <br>
-         <b style="margin-left:170px;">Дякуємо!</b><br>
+         <b>Дякуємо!</b><br>
          <b>Оплата пройшла успішно</b><br>
          <b>Підтвердження було вислано на ваш e-mail</b><br>
          <br>
