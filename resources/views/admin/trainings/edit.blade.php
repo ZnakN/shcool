f1db369958f1bd3b4bbf5818f1001fa505ccaad4
Aleksandr Neroznak <a.neroznak@itbsg.eu>
2017-07-13 20:16:56 +0300

    fix bug

--- resources/views/admin/trainings/edit.blade.php
+++ resources/views/admin/trainings/edit.blade.php
@@ -248,6 +248,7 @@ $(function()
                'adress_where':$('#adress_where').val(),
                'adress':$('#adress').val(),
                'full_price':$('#full_price').val(),
+               'one_price':$('#one_price').val(),
               'image':$('#thumbnail').val(),
               'meta_title':$('#meta_title').val(),
                'meta_description':$('#meta_description').val(),
